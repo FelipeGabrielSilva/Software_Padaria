5a469882543703c26b7e031feb2c6161f5e2fab3
Felipe <contatofelipedev@outlook.com>
2025-05-18 17:41:12 -0300

    feat(service): implementando service para pagamento

--- Services/Pagamento_Service.cs
+++ Services/Pagamento_Service.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsForm_Padaria.Model;
+using WindowsForm_Padaria.Resources;
 
 namespace WindowsForm_Padaria.Services
 {
     public class Pagamento_Service
     {
+        private readonly AppDbContext _context;
+        private readonly Validacao _valido;
+
+        public Pagamento_Service()
+        {
+            _context = new AppDbContext();
+            _valido = new Validacao();
+        }
+
+        public void Salvar(Pagamento p)
+        {
+            var res = _valido.Pagamento(p);
+
+            if (!res.Valido)
+            {
+                MessageBox.Show(string.Join("\n", res.Erros),"Erro ao cadastrar", MessageBoxButtons.OK);
+                return;
+            }
+
+            _context.Pagamento.Add(p);
+            _context.SaveChanges();
+        }
     }
 }
