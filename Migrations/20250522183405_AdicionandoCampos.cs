df920c65a2fa0010c9cb6a4fb4369d186e496ff9
Felipe <contatofelipedev@outlook.com>
2025-05-22 19:48:50 -0300

    new(migrations): nova migrations

---
+++ Migrations/20250522183405_AdicionandoCampos.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WindowsForm_Padaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoCampos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProdutosIds",
+                table: "Receita",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Categoria",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProdutosIds",
+                table: "Receita");
+
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Categoria");
+        }
+    }
+}
